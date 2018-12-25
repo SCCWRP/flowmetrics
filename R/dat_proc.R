283cef2039e1f5de48b5b1029cad8ef12f201966
fawda123 <mbafs2012@gmail.com>
2018-12-24 16:25:27 -0800

    added daily precipitation data from rnoaa package

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -3,6 +3,9 @@
 
 library(tidyverse)
 library(lubridate)
+library(foreach)
+library(doParallel)
+library(rnoaa)
 
 ##
 # flow metrics to predict
@@ -17,3 +20,48 @@ flowmet <- read.csv('raw/final_metrics.csv', stringsAsFactors = F) %>%
   )
 
 save(flowmet, file = 'data/flowmet.RData', compress = 'xz')
+
+######
+# get coarse daily estimates of precip for la
+# from rnoaa
+
+# days to select
+dts <- c('1985-01-01', '2014-12-31') %>% 
+  as.Date
+dts <- seq.Date(from = dts[1], to = dts[2], by = 'day')
+
+# la lat, lon, not used
+lalat <- 34.0522; lalon <- 360 - 118.2437 
+
+# setup clustering
+ncores <- detectCores() - 2  
+cl<-makeCluster(ncores)
+registerDoParallel(cl)
+strt<-Sys.time()
+
+# get gridded data, selected long/lat is approximate to la
+precip <- foreach(dt = seq_along(dts), .packages = c('tidyverse', 'rnoaa')) %dopar% {
+  
+  # log
+  sink('log.txt')
+  cat(dt, 'of', length(dts), '\n')
+  print(Sys.time()-strt)
+  sink()
+  
+  tmp <- cpc_prcp(dts[dt], us = T) %>% 
+    filter(lon == 241.875 & lat == 34.125)
+  
+  tmp$precip
+  
+}
+
+# precipitation (mm) with dates
+dayprcp <- data.frame(
+  date = dts, 
+  prcpmm = unlist(precip)
+  ) %>% 
+  mutate(
+    prcpmm = ifelse(prcpmm == -99.9, NA, prcpmm)
+  )
+
+save(dayprcp, file = 'data/dayprcp.RData', compress = 'xz')
