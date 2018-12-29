8c770c9cda687d4b665da54362e473ef3a329474
fawda123 <mbafs2012@gmail.com>
2018-12-29 10:49:01 -0800

    flowmetest in parallel

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -6,6 +6,8 @@ library(lubridate)
 library(foreach)
 library(doParallel)
 library(rnoaa)
+library(sf)
+library(randomForest)
 
 ##
 # flow metrics to predict
@@ -89,3 +91,175 @@ yrprcp <- dayprcp %>%
   )
 
 save(yrprcp, file = 'data/yrprcp.RData', compress = 'xz')
+
+######
+# random forest models of flow metrics
+
+data(flowmet)
+data(yrprcp)
+data(comid_atts)
+
+# remote geometry from comid
+comid_atts <- comid_atts %>% 
+  st_set_geometry(NULL)
+
+# remove totprcp column
+yrprcp <- yrprcp %>% 
+  select(-totprcp)
+
+# setup data to model
+# includes folds, need to make sure these are the same folds across each metric
+tomod <- flowmet %>% 
+  mutate(
+    mo = month(date),
+    yr = year(date)
+  ) %>% 
+  group_by(mo) %>% 
+  mutate(folds = sample(1:5, length(mo), replace = T)) %>% 
+  ungroup %>% 
+  gather('var', 'val', -watershedID, -COMID, -date, -mo, -yr, -folds) %>% 
+  # filter(var %in% metsel) %>% 
+  left_join(comid_atts, by = 'COMID') %>% 
+  left_join(yrprcp, by = 'yr') %>% 
+  group_by(var, mo, catprcp) %>% 
+  nest
+
+# setup parallel
+ncores <- detectCores() - 1 
+cl<-makeCluster(ncores)
+registerDoParallel(cl)
+strt<-Sys.time()
+
+# create models
+mods <- foreach(rw = 1:nrow(tomod), .packages = c('randomForest', 'tidyverse')) %dopar% {
+
+  # log
+  sink('log.txt')
+  cat(rw, 'of', nrow(tomod), '\n')
+  print(Sys.time()-strt)
+  sink()
+  
+  # get index selections
+  var <- tomod[rw, 'var']
+  mo <- tomod[rw, 'mo']
+  catprcp <- as.character(tomod[rw, 'catprcp'])
+  data <- tomod$data[[rw]]
+  
+  # model formula, all
+  frm <- names(data)[!names(data) %in% c('watershedID', 'COMID', 'date', 'yr', 'folds', 'var', 'val')] %>% 
+    paste(collapse = ' + ') %>% 
+    paste0('val ~ ', .) %>% 
+    formula
+  
+  # folds
+  flds <- unique(data$folds)
+  
+  # pre-allocated output
+  out <- vector('list', length = length(flds))
+  
+  # loop through folds
+  for(fld in flds){
+    
+    # calibration data
+    calset <- data %>% 
+      filter(!folds %in% fld)
+    
+    # validation data
+    valset <- data %>% 
+      filter(folds %in% fld)
+    
+    # create model
+    mod <- randomForest(frm, data = calset, ntree = 1000, importance = TRUE, na.action = na.omit, keep.inbag = TRUE)
+    
+    # top ten important variables
+    impvars <- mod$importance %>% 
+      as.data.frame %>% 
+      tibble::rownames_to_column('var') %>% 
+      arrange(-`%IncMSE`) %>% 
+      pull(var) %>% 
+      .[1:10]
+    
+    # new formula, from top ten
+    frmimp <- impvars %>% 
+      paste(collapse = '+') %>% 
+      paste0('val~', .) %>% 
+      formula
+    
+    # create model, from top ten
+    modimp <- randomForest(frmimp, data = calset, ntree = 1000, importance = TRUE, na.action = na.omit, keep.inbag = TRUE)
+    
+    # oob predictions for mod, modimp
+    calsetid <- calset %>% 
+      mutate(id = 1:nrow(.)) %>% 
+      select(id)
+    prd <- predict(mod) %>% 
+      data.frame(prd = .) %>% 
+      rownames_to_column('id') %>% 
+      mutate(id = as.numeric(id)) %>% 
+      left_join(calsetid, ., by = 'id') %>% 
+      pull(prd)
+    prdimp <- predict(modimp) %>% 
+      data.frame(prd = .) %>% 
+      rownames_to_column('id') %>% 
+      mutate(id = as.numeric(id)) %>% 
+      left_join(calsetid, ., by = 'id') %>% 
+      pull(prd)
+    
+    # calibration prediction, full and important
+    calpred <- tibble(
+      set = 'cal',
+      COMID = calset$COMID, 
+      date= calset$date,
+      obs = calset$val, 
+      prd = prd,
+      prdimp = prdimp
+    )
+    
+    # validation prediction, full and important
+    valpred <- tibble(
+      set = 'val', 
+      COMID = valset$COMID,
+      date = valset$date,
+      obs = valset$val, 
+      prd = predict(mod, newdata = valset),
+      prdimp = predict(modimp, newdata = valset)
+    )
+    
+    # combine cal, val, get summary stats
+    prds <- bind_rows(calpred, valpred) %>% 
+      gather('modtyp', 'modprd', prd, prdimp) %>% 
+      group_by(set, modtyp) %>% 
+      nest() %>% 
+      mutate(
+        rmse = map(data, function(x) sqrt(mean(x$obs - x$modprd, na.rm = T)^2)), 
+        rsqr = map(data, function(x){
+          lm(obs ~ modprd, data = x) %>% 
+            summary %>%
+            .$r.squared
+        }), 
+        impvars = list(impvars)
+      ) %>% 
+      unnest(rmse, rsqr)
+    
+    # append to output
+    out[[fld]] <- prds
+    
+  }
+  
+  # combine all fold data
+  out <- out %>% 
+    enframe('fld') %>% 
+    unnest
+  
+  return(out)
+    
+}
+
+# final output
+flowmetest <- tomod %>% 
+  select(-data) %>% 
+  bind_cols(., enframe(mods)) %>% 
+  select(-name) %>% 
+  unnest(value)
+
+save(flowmetest, file = 'data/flowmetest.RData', compress = 'xz')
