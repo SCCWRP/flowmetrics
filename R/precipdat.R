b6fe6e20d461c6afa6d47537a96c0d0a64a53485
fawda123 <mbafs2012@gmail.com>
2019-01-14 17:29:33 -0800

    working on precip proc baseline

--- R/precipdat.R
+++ R/precipdat.R
@@ -1,31 +1,84 @@
 library(h5)
+library(tidyverse)
+library(sf)
+library(foreach)
+library(doParallel)
 
-# setwd to rcp45 scenario
-setwd('L:/Flow ecology and climate change_ES/Data/RawData/CCSM4/rcp45')
+utmprj <- "+proj=utm +zone=11 +datum=NAD83 +units=m +no_defs"
+decprj <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 
-# get file list of yearly estimates
-fls <- dir()
+data(comid_pnts)
 
-# get first file
-fl <- fls[1] 
+# reproject
+comid_sel <- comid_pnts %>% 
+  st_transform(decprj)
 
-# extract contents to wd, subdir tmp
-untar(fl, exdir = 'tmp')
+# lat and lon
+# lats <- readLines('L:/Flow ecology and climate change_ES/Data/RawData/Lat.dat') %>% 
+#   strsplit('\\s+') %>% 
+#   unlist %>% 
+#   unique %>% 
+#   as.numeric %>% 
+#   range(na.rm = T)
+# lons <- readLines('L:/Flow ecology and climate change_ES/Data/RawData/Lon.dat') %>% 
+#   strsplit('\\s+') %>% 
+#   unlist %>% 
+#   unique %>% 
+#   as.numeric %>% 
+#   range(na.rm = T)
+lats <- c(33.5, 35.01101)
+lons <- c(-119.70, -117.37)
 
-# get h5 file names in extracted directory
-h5fls <- list.files('tmp', recursive = T, full.names = T)
+# get recursive file list, daily for each water year
+fls <- list.files('D:/Baseline', recursive = T, full.names = T)
 
-# select one h5 file, open connection
-h5fl <- h5fls[1]
-h5flcon <- h5file(name = h5fl, mode = "a")
+# setup parallel backend
+ncores <- detectCores() - 1  
+cl<-makeCluster(ncores)
+registerDoParallel(cl)
+strt<-Sys.time()
 
-# read dataset, 3hr x 1600 x 2400
-ppt <- readDataSet(h5flcon['PPT'])
+# process
+res <- foreach(i = seq_along(fls), .packages = c('tidyverse', 'sf', 'raster', 'h5')) %dopar% {
 
-# plot hour 1
-image(ppt[1, ,])
+  # log
+  sink('log.txt')
+  cat(i, 'of', length(fls), '\n')
+  print(Sys.time()-strt)
+  sink()
 
-# questions
-# what are precip units, mm?
-# what is bounding box? 
-# what are three hour time steps? 
+  # select one h5 file, open connection
+  h5flcon <- h5file(name = fls[i], mode = "a")
+  
+  # read dataset, 8(3hr) x 1600 x 2400
+  ppt <- readDataSet(h5flcon['PPT'])
+  
+  # extract three hour estimates for the day
+  dly <- NULL
+  for(i in 1:nrow(ppt)){
+  
+    # convert to raster, project, extract
+    ppttmp <- ppt[i, , ] %>% 
+      .[nrow(.):1, ] %>% 
+      raster(xmn = lons[1], xmx = lons[2], ymn = lats[1], ymx = lats[2], crs = CRS(decprj)) %>% 
+      extract(comid_sel)
+  
+    # combine three hour ests
+    dly <- cbind(dly, ppttmp)
+    
+  }
+
+  # get daily total, combine with COMID
+  dly <- rowSums(dly, na.rm = T) %>% 
+    tibble(COMID = comid_pnts$COMID, dly_prp = .)
+  
+  return(dly)
+  
+}
+
+# combine output to save
+names(res) <- basename(fls[1:50])
+bsext <- res %>% 
+  enframe %>% 
+  unnest
+save(bsext, file = 'data/bsext.RData', compress = 'xz')
--- README.md
+++ README.md
@@ -12,6 +12,8 @@ tenyr, twoyr, X5yrRBI, x3_SFR, all_R10D.5, x3_QmaxIDR, x5_RecessMaxLength, all_L
 
 * `comid_atts.RData` sf object of NHDplus flowlines and joined StreamCat data.  NHDplus flowlines were filtered to remove those in "unnatural settings" (by clustering of land use and presence of dam in watershed).  StreamCat data were filtered to remove those with near zero variance for the region (i.e., no predictive power) or they were redundant.  This file was created using source data outside of this project with `R/COMID clustering.R` from JT.
 
+* `comid_pnts.RData` sf object of NHDplus flowline centroids, same as `comid_atts.RData` from `R/COMID clustering.R`
+
 * `flowmet.RData` Flow metric data to model, includes data from above list taken from raw data file
 
 * `flowmetprf.RData` Model performance for estimated flow metrics
